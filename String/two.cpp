2c86deabb239351bf261e9341e2c2c1d19dcfbed
Rishabh-020 <rishabh1813.becse24@chitkara.edu.in>
2025-07-02 15:11:21 +0530

    number to binary

--- String/two.cpp
+++ String/two.cpp
@@ -1,10 +1,29 @@
 #include<iostream>
 #include<string>
 using namespace std;
+int toBinary(int num){
+    int temp=num,len=0;
+    while(temp>0){
+        ++len;
+        temp/=2;
+    }
+    int arr[len],i=0;
+    while(num>0){
+        arr[i++]=num%2;
+        num/=2;
+    }
+    int binary=0;
+    for(int i=len-1;i>=0;i--){
+        binary=binary*10+arr[i];
+    }
+    return binary;
+}
 int main(){
-    int a=1;
-    int b=a++;
-    int c=++a;
-    cout<<a<<" "<<b<<" "<<c;
-    
+    // int a=1;
+    // int b=a++;
+    // int c=++a;
+    // cout<<a<<" "<<b<<" "<<c;
+    int num;
+    cin>>num;
+    cout<<toBinary(num);
 }
