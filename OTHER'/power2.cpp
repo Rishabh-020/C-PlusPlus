835a9c3757bf10d2fea7a1d0d819757155c2c777
Rishabh-020 <rishabh1813.becse24@chitkara.edu.in>
2025-07-07 14:29:56 +0530

    done

--- OTHER'/power2.cpp
+++ OTHER'/power2.cpp
@@ -1,19 +1,25 @@
 #include<iostream>
 using namespace std;
-class power{
-public:
-    bool isPower(int num){
-        if(num<=0)return false;
-        while(num%2==0){
-            num=num>>1;
-        }
-        return num==1;
-    }
-};
+// class power{
+// public:
+//     bool isPower(int num){
+//         if(num<=0)return false;
+//         while(num%2==0){
+//             num=num>>1;
+//         }
+//         return num==1;
+//     }
+// };
+//Slove the question to find how many number of notes of hundred,fifty,twenty,one are req to get the value(num)
 int main(){
     int num;
     cin>>num;
-    power p;
-    cout<<p.isPower(num);
+    // power p;
+    // cout<<p.isPower(num);
+    switch(num){
+        int hundred=num%100;
+        int fifty=(num-(hundred*100))%50;
+        int twenty= (num-)
+    }
     return 0;
 }
