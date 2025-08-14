c730612a31aab662c9b7681286f9c393643ff9ad
Rishabh-020 <rishabh1813.becse24@chitkara.edu.in>
2025-08-14 09:56:33 +0530

    .

--- OOPS_CPP/constant/constObj.cpp
+++ OOPS_CPP/constant/constObj.cpp
@@ -4,10 +4,15 @@ class obj{
 public:
     int num;
     string name;
+    const float marks=0.0;
     obj(){
+        // marks=12;
         num=1;
     };
     obj(int val): num(val){};
+    void display(){
+        cout<<"This is not a constant function"<<endl;
+    }
     void call() const{
     cout<<"Age of "<<name<<" is "<<num<<endl;
     }
@@ -21,6 +26,7 @@ int main(){
     obj o1(12);
     const obj o2;
     o2.call();
+    o1.display();
     o1.call();
     return 0;
 }
