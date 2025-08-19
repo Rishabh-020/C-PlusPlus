707aa21ba4e1ee2afa227cb5fd7969bf345ba15e
Rishabh-020 <rishabh1813.becse24@chitkara.edu.in>
2025-08-19 11:31:52 +0530

    .

---
+++ OOPS_CPP/static/mcq.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+class Player
+{
+private:
+    int id;
+    static int next_id;
+
+public:
+Player() { id = next_id++; }
+int getID() { return id; }
+};
+int Player::next_id = 1;
+int main()
+{
+    Player p1;
+    Player p2;
+    Player p3;
+    cout << p1.getID() << " ";
+    cout << p2.getID() << " ";
+    cout << p3.getID();
+    return 0;
+}
